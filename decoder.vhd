6926584bf27d144ba100fb42bb4884c539fd59bf
Fosfor Aaka <fosfor.software@seznam.cz>
2016-10-17 07:58:21 +0200

    estetika, logicka posloupnost:)

--- decoder.vhd
+++ decoder.vhd
@@ -23,16 +23,13 @@ BEGIN
 	PROCESS(clk)
 	BEGIN
 		IF(rising_edge(clk)) THEN
-			A(3) <= A(2);
-			B(3) <= B(2);
-			A(2) <= A(1);
-			B(2) <= B(1);
-			A(1) <= A(0);
-			B(1) <= B(0);
-			A(0) <= encoder_A;
-			B(0) <= encoder_B;
+			A <= A(2 downto 0) & encoder_A;
+			B <= B(2 downto 0) & encoder_B;
 		END IF;
 	END PROCESS;
+
+	pulse <= A(3) XOR A(2) XOR B(3) XOR B(2);
+	dir <= A(3) XOR B(2);
 
 	--pocitani pulzu
 	PROCESS(pulse, dir, reset)
@@ -48,8 +45,6 @@ BEGIN
 		END IF;
 	END PROCESS;
 
-	pulse <= A(3) XOR A(2) XOR B(3) XOR B(2);
-	dir <= A(3) XOR B(2);
 	position <= count;
 
 END logic;
